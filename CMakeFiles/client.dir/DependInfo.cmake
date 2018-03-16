d685103df465da4ad82609975916b1a340defeb5
marcmadalin <madalin.marc@busymachines.com>
2018-03-16 16:55:24 +0200

    fix

---
+++ CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/marcmadalin/Desktop/soc/Multi-Segment-File-Transfer/client.c" "/Users/marcmadalin/Desktop/soc/Multi-Segment-File-Transfer/CMakeFiles/client.dir/client.c.o"
+  "/Users/marcmadalin/Desktop/soc/Multi-Segment-File-Transfer/file_utilities.c" "/Users/marcmadalin/Desktop/soc/Multi-Segment-File-Transfer/CMakeFiles/client.dir/file_utilities.c.o"
+  "/Users/marcmadalin/Desktop/soc/Multi-Segment-File-Transfer/netio.c" "/Users/marcmadalin/Desktop/soc/Multi-Segment-File-Transfer/CMakeFiles/client.dir/netio.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
